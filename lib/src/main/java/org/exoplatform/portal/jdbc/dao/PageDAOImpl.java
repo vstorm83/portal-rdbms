a4f951ed56eefdbbdb9864b98770405e1f798406
phuong_vu <phuongvv@exoplatform.com>
2016-05-14 12:14:41 +0700

    EXOGTN-2198 Implement JPA storage for token service

--- lib/src/main/java/org/exoplatform/portal/jdbc/dao/PageDAOImpl.java
+++ lib/src/main/java/org/exoplatform/portal/jdbc/dao/PageDAOImpl.java
@@ -24,87 +24,89 @@ import org.exoplatform.portal.mop.page.PageKey;
 
 public class PageDAOImpl extends AbstractDAO<PageEntity> implements PageDAO {
 
-    @Override
-    public PageEntity findByKey(PageKey pageKey) {
-        TypedQuery<PageEntity> query = getEntityManager().createNamedQuery("PageEntity.findByKey", PageEntity.class);
+  @Override
+  public PageEntity findByKey(PageKey pageKey) {
+    TypedQuery<PageEntity> query = getEntityManager().createNamedQuery("PageEntity.findByKey",
+                                                                       PageEntity.class);
 
-        SiteKey siteKey = pageKey.getSite();
-        query.setParameter("ownerType", siteKey.getType());
-        query.setParameter("ownerId", siteKey.getName());
-        query.setParameter("name", pageKey.getName());
-        try {
-            return query.getSingleResult();
-        } catch (NoResultException ex) {
-            return null;
-        }
+    SiteKey siteKey = pageKey.getSite();
+    query.setParameter("ownerType", siteKey.getType());
+    query.setParameter("ownerId", siteKey.getName());
+    query.setParameter("name", pageKey.getName());
+    try {
+      return query.getSingleResult();
+    } catch (NoResultException ex) {
+      return null;
     }
+  }
 
-    @Override
-    public ListAccess<PageEntity> findByQuery(PageQuery query) {
-        TypedQuery<PageEntity> q = buildQuery(query);
-        final List<PageEntity> results = q.getResultList();
+  @Override
+  public ListAccess<PageEntity> findByQuery(PageQuery query) {
+    TypedQuery<PageEntity> q = buildQuery(query);
+    final List<PageEntity> results = q.getResultList();
 
-        return new ListAccess<PageEntity>() {
+    return new ListAccess<PageEntity>() {
 
-            @Override
-            public int getSize() throws Exception {
-                return results.size();
-            }
-
-            @Override
-            public PageEntity[] load(int offset, int limit) throws Exception, IllegalArgumentException {
-              if (limit < 0) {
-                limit = getSize();
-              }
-              return results.subList(offset, offset + limit).toArray(new PageEntity[limit]);                
-            }
-        };
-    }
-
-    public TypedQuery<PageEntity> buildQuery(PageQuery query) {
-        EntityManager em = EntityManagerHolder.get();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<PageEntity> criteria = cb.createQuery(PageEntity.class);
-        Root<PageEntity> pageEntity = criteria.from(PageEntity.class);
-        //
-        CriteriaQuery<PageEntity> select = criteria.select(pageEntity);
-        select.distinct(true);
-
-        List<Predicate> predicates = new LinkedList<Predicate>();
-
-        if (query.getSiteType() != null) {
-            predicates.add(cb.equal(pageEntity.get(PageEntity_.ownerType), convertSiteType(query.getSiteType())));
-        }
-        if (query.getSiteName() != null) {
-            predicates.add(cb.equal(pageEntity.get(PageEntity_.ownerId), query.getSiteName()));
-        }
-
-        if (query.getDisplayName() != null) {
-            predicates.add(cb.like(cb.lower(pageEntity.get(PageEntity_.displayName)),
-                    "%" + query.getDisplayName().toLowerCase() + "%"));
-        }
-
-        select.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
-
-        //
-        TypedQuery<PageEntity> typedQuery = em.createQuery(select);
-        Pagination pagination = query.getPagination();
-        if (pagination != null && pagination.getLimit() > 0) {
-            typedQuery.setFirstResult(pagination.getOffset());
-            typedQuery.setMaxResults(pagination.getLimit());
-        }
-        //
-        return typedQuery;
-    }
-
-    private org.exoplatform.portal.mop.SiteType convertSiteType(SiteType siteType) {
-      switch (siteType) {
-      case SITE:
-        return org.exoplatform.portal.mop.SiteType.PORTAL;
-      case SPACE:
-        return org.exoplatform.portal.mop.SiteType.GROUP;
-      default:
-        return org.exoplatform.portal.mop.SiteType.USER;
+      @Override
+      public int getSize() throws Exception {
+        return results.size();
       }
+
+      @Override
+      public PageEntity[] load(int offset, int limit) throws Exception, IllegalArgumentException {
+        if (limit < 0) {
+          limit = getSize();
+        }
+        return results.subList(offset, offset + limit).toArray(new PageEntity[limit]);
+      }
+    };
+  }
+
+  public TypedQuery<PageEntity> buildQuery(PageQuery query) {
+    EntityManager em = EntityManagerHolder.get();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<PageEntity> criteria = cb.createQuery(PageEntity.class);
+    Root<PageEntity> pageEntity = criteria.from(PageEntity.class);
+    //
+    CriteriaQuery<PageEntity> select = criteria.select(pageEntity);
+    select.distinct(true);
+
+    List<Predicate> predicates = new LinkedList<Predicate>();
+
+    if (query.getSiteType() != null) {
+      predicates.add(cb.equal(pageEntity.get(PageEntity_.ownerType),
+                              convertSiteType(query.getSiteType())));
     }
+    if (query.getSiteName() != null) {
+      predicates.add(cb.equal(pageEntity.get(PageEntity_.ownerId), query.getSiteName()));
+    }
+
+    if (query.getDisplayName() != null) {
+      predicates.add(cb.like(cb.lower(pageEntity.get(PageEntity_.displayName)),
+                             "%" + query.getDisplayName().toLowerCase() + "%"));
+    }
+
+    select.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+
+    //
+    TypedQuery<PageEntity> typedQuery = em.createQuery(select);
+    Pagination pagination = query.getPagination();
+    if (pagination != null && pagination.getLimit() > 0) {
+      typedQuery.setFirstResult(pagination.getOffset());
+      typedQuery.setMaxResults(pagination.getLimit());
+    }
+    //
+    return typedQuery;
+  }
+
+  private org.exoplatform.portal.mop.SiteType convertSiteType(SiteType siteType) {
+    switch (siteType) {
+    case SITE:
+      return org.exoplatform.portal.mop.SiteType.PORTAL;
+    case SPACE:
+      return org.exoplatform.portal.mop.SiteType.GROUP;
+    default:
+      return org.exoplatform.portal.mop.SiteType.USER;
+    }
+  }
 }
